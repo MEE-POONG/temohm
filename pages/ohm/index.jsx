563ba063e35b58983e2ca8257f359804103fc9be
BAdGHost201 <oooui201@gmail.com>
2022-08-10 15:40:07 +0700

    ohm

--- pages/ohm/index.jsx
+++ pages/ohm/index.jsx
@@ -1,26 +1,29 @@
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-// import ModalAddEmpolyee from '../../components/Modal/ModalAddEmployee';
-// import ModalEditEmployee from '../../components/Modal/ModalEditEmployee';
-import { Button, OverlayTrigger, Tooltip, Row, Col, Image } from 'react-bootstrap';
-// import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { Button, OverlayTrigger, Tooltip, Row, Col, Image, Container, Table } from 'react-bootstrap';
+
 
 
 function Index() {
     return (
-        <Container className="my-5 body">
+
+        <Container >
             <Row>
-                <Col>
-                    <Image src="https://i.imgur.com/hKOhIPt.jpeg" />
-                    <h1>
-                        how to nbackaflemguijrm
-                    </h1>
-                    <p>
-                        my ass
-                    </p>
+                <Col className='fonthead'>
+                    <div>
+                        <h1>sad</h1>
+                        <p className='fontsubtitle fontsupersize'>
+                            i like your ass ไอเหี้ย
+                        </p>
+                    </div>
+                </Col>
+                <Col className='fonthead '>
+                    <div>
+                        <h2>sad</h2>
+                        <p className='fontsubtitle fontsupersize'>asdsadsada</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
+
     );
 }
 
