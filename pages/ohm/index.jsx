f379bba7263698d97adfaa042ee845c97404c1c9
BAdGHost201 <oooui201@gmail.com>
2022-08-10 14:15:26 +0700

    ohm

---
+++ pages/ohm/index.jsx
@@ -0,0 +1,27 @@
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+// import ModalAddEmpolyee from '../../components/Modal/ModalAddEmployee';
+// import ModalEditEmployee from '../../components/Modal/ModalEditEmployee';
+import { Button, OverlayTrigger, Tooltip, Row, Col, Image } from 'react-bootstrap';
+// import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+
+
+function Index() {
+    return (
+        <Container className="my-5 body">
+            <Row>
+                <Col>
+                    <Image src="https://i.imgur.com/hKOhIPt.jpeg" />
+                    <h1>
+                        how to nbackaflemguijrm
+                    </h1>
+                    <p>
+                        my ass
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Index;
