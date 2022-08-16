48b59d97f53bc8ac434279c1e55b1d24912eb957
BAdGHost201 <oooui201@gmail.com>
2022-08-16 13:14:02 +0700

    ohm

--- pages/ohm/index.jsx
+++ pages/ohm/index.jsx
@@ -5,23 +5,39 @@ import { Button, OverlayTrigger, Tooltip, Row, Col, Image, Container, Table } fr
 function Index() {
     return (
 
-        <Container >
-            <Row>
-                <Col className='fonthead'>
-                    <div>
-                        <h1>sad</h1>
-                        <p className='fontsubtitle fontsupersize'>
-                            i like your ass ไอเหี้ย
-                        </p>
-                    </div>
-                </Col>
-                <Col className='fonthead '>
-                    <div>
-                        <h2>sad</h2>
-                        <p className='fontsubtitle fontsupersize'>asdsadsada</p>
-                    </div>
-                </Col>
-            </Row>
+        <Container className='my-5 '>
+            <section className='bgset-998'>
+
+
+
+                <div className='anotherbackground'>
+
+                    <Row>
+                        <Col>
+
+                        </Col>
+                        <Col>
+                            <h1 className='fonthead fontsupersize' >Making Your World a Happier Place</h1>
+                            <p className='fontsubtitle '>
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                                Images from
+                                <Button variant="outline-primary">Read more</Button>
+                            </p>
+                        </Col>
+                    </Row>
+
+                </div>
+                <div className='img-profile'>
+
+                    <Image src="https://i.imgur.com/PEgflZ4.jpeg" className='img' />
+
+                </div>
+                <div className='img-bg'>
+                    <Image src="https://i.imgur.com/SRvOobE.png" className='imgbg' />
+
+                </div>
+            </section>
         </Container>
 
     );
