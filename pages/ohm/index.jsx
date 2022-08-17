ee7e5591c96144eb85d88d862a520a4ba8922d68
BAdGHost201 <oooui201@gmail.com>
2022-08-17 15:26:03 +0700

    ohm

--- pages/ohm/index.jsx
+++ pages/ohm/index.jsx
@@ -1,4 +1,4 @@
-import { Button, OverlayTrigger, Tooltip, Row, Col, Image, Container, Table } from 'react-bootstrap';
+import { Button, OverlayTrigger, Tooltip, Row, Col, Image, Container, Table, box } from 'react-bootstrap';
 import { GrDiamond } from 'react-icons/gr';
 
 
@@ -8,13 +8,13 @@ function Index() {
         <Container className='my-5 '>
             <section className='bgset-998'>
 
-
+        
 
                 <div className='anotherbackground'>
 
                     <Row>
                         <Col>
-                        
+
                         </Col>
                         <Col>
                             <h1 className='fonthead fontsupersize' >Making Your World a Happier Place</h1>
@@ -24,7 +24,7 @@ function Index() {
                             <p>
                                 <Button variant="outline-primary" className='button'>Read more</Button>
                             </p>
-                            
+
 
                         </Col>
                     </Row>
@@ -33,7 +33,7 @@ function Index() {
                 </div>
                 <div className='img-profile'>
 
-                    <Image src="https://i.imgur.com/PEgflZ4.jpeg" className='img' />
+                    <Image src="https://i.imgur.com/PEgflZ4.jpeg" className='imge' />
 
                 </div>
                 <div className='img-bg'>
@@ -54,10 +54,15 @@ function Index() {
 
                         </Col>
                         <Col>
-                        <h1 className='fontheader'>business</h1>
-                            
+                            <Image src="https://i.imgur.com/PHCR2vy.png" className="img" />
+                            <h1 className='fontheader'>business</h1>
+
+                            <p className='fontsub'>Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                        </Col>
+                        <Col>
 
                         </Col>
+
                     </Row>
 
 
@@ -76,35 +81,22 @@ function Index() {
 
 
 
-                <div className='anotherbackground'>
-
+                <div className=''>
+                    <h1 className='fontheader-get'>Simple prices, flexible    options & nothing hidden</h1>
                     <Row>
                         <Col>
-
+                            <h1 className='fontsub-get'>free</h1>
                         </Col>
                         <Col>
-                            <h1 className='fonthead fontsupersize' >Making Your World a Happier Place</h1>
-                            <p className='fontsubtitle '>
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                            <p>
-                                <Button variant="outline-primary" className='button'>Read more</Button>
-                            </p>
-
-
+                            <h1 className='fontsub-get'>free</h1>
                         </Col>
+                        <Col>
+                            <h1 className='fontsub-get'>free</h1>
+                        </Col>
+
+
+
                     </Row>
-
-
-                </div>
-                <div className='img-profile'>
-
-                    <Image src="https://i.imgur.com/PEgflZ4.jpeg" className='img' />
-
-                </div>
-                <div className='img-bg'>
-                    <Image src="https://i.imgur.com/SRvOobE.png" className='imgbg' />
-
                 </div>
             </section>
         </Container>
